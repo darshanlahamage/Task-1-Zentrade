64e873f58e161dfbc5397a86694def294293cac6
darshanlahamage <80032609+darshanlahamage@users.noreply.github.com>
2024-01-08 14:07:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import smartserv from './smartserv.jpg'
 
-function App() {
+const App = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get('https://s3.amazonaws.com/open-to-cors/assignment.json');
+        const sortedProducts = Object.values(response.data.products).sort((a, b) => b.popularity - a.popularity);
+        setProducts(sortedProducts);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="logo">
+        <img src={smartserv} alt="Logo" />
+      </div>
+      <div className="product-table-container">
+        <h1>Task 1 - Product Display</h1>
+        <table className="product-table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Price</th>
+              <th>Popularity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product, index) => (
+              <tr key={index}>
+                <td>{product.title}</td>
+                <td>${product.price}</td>
+                <td>{product.popularity}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
